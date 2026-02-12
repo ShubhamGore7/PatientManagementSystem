ac153f4dfdbf634c9a12411a5b78783f05e5e228
ShubhamGore <shubhamgore@ShubhamGores-MacBook-Air.local>
2026-02-12 14:40:22 +0530

    Updated API URL

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { PatientRegistrationComponent } from './patient-registration/patient-registration';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: 'register', component: PatientRegistrationComponent }
+];
